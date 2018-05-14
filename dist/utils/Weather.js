5d7581428cfd6fea173e4c74792da36545db3207
Nomsy <me@nomsy.net>
2018-05-14 16:39:56 -0400

    Removed dist folder

--- dist/utils/Weather.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var WeatherCodes;
-(function (WeatherCodes) {
-})(WeatherCodes || (WeatherCodes = {}));
-class Weather {
-}
-exports.Weather = Weather;
